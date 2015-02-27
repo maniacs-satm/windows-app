9a725c099757dc62763c3c2b49200c6472bce9ff
Julian Oster <github@jlnostr.de>
2015-02-27 20:35:15 +0100

    Comments for ApplicationSettings.cs

--- wallabag/wallabag.Shared/Common/ApplicationSettings.cs
+++ wallabag/wallabag.Shared/Common/ApplicationSettings.cs
@@ -4,19 +4,28 @@ using System.ComponentModel;
 
 namespace wallabag.Common
 {
+    /// <summary>
+    /// This class provides easy access to the roaming settings (sync over multiple devices).
+    /// </summary>
     public class ApplicationSettings : INotifyPropertyChanged
     {
+        // This ugly code block allows it to access on the same Instance even if we use the 'static' parameter.
         private static ApplicationSettings _instance;
         private ApplicationSettings() { }
         public static ApplicationSettings Instance { get { return _instance ?? (_instance = new ApplicationSettings()); } }
 
         private ApplicationDataContainer roamingSettings = ApplicationData.Current.RoamingSettings;
 
+        /// <summary>
+        /// Why a ObservableDictionary instead of instant access?
+        /// Because it's faster.
+        /// </summary>
         private ObservableDictionary _settings;
         public ObservableDictionary Settings
         {
             get
             {
+                // For the first access on the settings, we load the data from the current sync state.
                 if (_settings == null || _settings.Count == 0)
                 {
                     _settings = new ObservableDictionary();
@@ -29,6 +38,12 @@ namespace wallabag.Common
             }
         }
 
+        /// <summary>
+        /// Allows easier access to the Settings and saves them directly in both the ObservableDictionary and the ApplicationDataContainer.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
         public dynamic this[string key, object defaultValue = default(object)]
         {
             get
@@ -47,6 +62,10 @@ namespace wallabag.Common
             }
         }
 
+        /// <summary>
+        /// To inform the View about a changed property, we raise a PropertyChangedEvent.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
         private void RaisePropertyChanged(string propertyName)
         {
             if (PropertyChanged != null)
