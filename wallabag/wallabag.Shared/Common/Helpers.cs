21bb1446af21e2f55e923156237c28ab83158ae0
Julian Oster <github@jlnostr.de>
2015-02-27 20:41:06 +0100

    Comments for Helpers.cs

--- wallabag/wallabag.Shared/Common/Helpers.cs
+++ wallabag/wallabag.Shared/Common/Helpers.cs
@@ -4,6 +4,11 @@ namespace wallabag.Common
 {
     public static class Helpers
     {
+        /// <summary>
+        /// There are several languages. To access them from code-behind this way is required.
+        /// </summary>
+        /// <param name="resourceName">The name of the resource in the dictionary.</param>
+        /// <returns>A string representing the translation for the current language.</returns>
         public static string LocalizedString(string resourceName)
         {
             return ResourceLoader.GetForCurrentView().GetString(resourceName);
