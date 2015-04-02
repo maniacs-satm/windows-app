74482b287330b253c2fac78a50f77df53408d03c
Julian Oster <github@jlnostr.de>
2015-04-02 17:08:40 +0200

    Add missing MainViewModel.cs

---
+++ wallabag/wallabag.Shared/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using wallabag.Common;
+using wallabag.DataModel;
+
+namespace wallabag.ViewModels
+{
+    public class MainViewModel
+    {
+        public RelayCommand refreshCommand { get; private set; }
+        private async Task RefreshItems()
+        {
+            var items = await wallabagDataSource.GetItemsAsync();
+        }
+
+        public MainViewModel()
+        {
+            refreshCommand = new RelayCommand(async () => await RefreshItems());
+            refreshCommand.Execute(0);
+        }
+    }
+}
