a3430e82353e4621aba6c789e7442ade51350f7b
Julian Oster <github@jlnostr.de>
2015-04-02 14:48:51 +0200

    Implement ItemViewModel.

---
+++ wallabag/wallabag.Shared/DataModel/ItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PropertyChanged;
+using Newtonsoft.Json;
+
+namespace wallabag.DataModel
+{
+    [ImplementPropertyChanged]
+    public class ItemViewModel
+    {
+        public Item Model { get; set; }
+
+    }
+}
