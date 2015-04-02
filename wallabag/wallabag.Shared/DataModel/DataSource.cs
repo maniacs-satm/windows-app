c27af15aa5c44a834ec3f282e42a261679a9218d
Julian Oster <github@jlnostr.de>
2015-04-02 21:51:31 +0200

    Implement GetItemAsync method.

--- wallabag/wallabag.Shared/DataModel/DataSource.cs
+++ wallabag/wallabag.Shared/DataModel/DataSource.cs
@@ -21,41 +21,40 @@ namespace wallabag.DataModel
 
         public static ObservableCollection<ItemViewModel> Items { get; set; }
 
-        public static async Task<bool> GetItemsAsync(int page = 1)
+        public static async Task<bool> GetItemsAsync(int page = 1, bool IsSingleItem = false)
         {
-            HttpClient http = new HttpClient();
-
-            await Helpers.AddHeaders(http, new User() { Username = "wallabag", Password = "wallabag" });
-            System.Diagnostics.Debug.WriteLine(http.DefaultRequestHeaders["X-WSSE"]);
-            var response = await http.GetAsync(new Uri(string.Format("http://v2.wallabag.org/api/entries.json?page={0}", page)));
-            http.Dispose();
-
-            if (response.StatusCode == HttpStatusCode.Ok ||
-                response.StatusCode == HttpStatusCode.NoContent)
+            if (!Helpers.IsConnectedToInternet() || IsSingleItem)
+                await RestoreItemsAsync();
+            else
             {
-                List<Item> items = await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<List<Item>>(response.Content.ToString()));
-                Items = new ObservableCollection<ItemViewModel>();
-                foreach (Item i in items)
-                    Items.Add(new ItemViewModel(i));
-                return true;
+                HttpClient http = new HttpClient();
+
+                await Helpers.AddHeaders(http, new User() { Username = "wallabag", Password = "wallabag" });
+                System.Diagnostics.Debug.WriteLine(http.DefaultRequestHeaders["X-WSSE"]);
+                var response = await http.GetAsync(new Uri(string.Format("http://v2.wallabag.org/api/entries.json?page={0}", page)));
+                http.Dispose();
+
+                if (response.StatusCode == HttpStatusCode.Ok ||
+                    response.StatusCode == HttpStatusCode.NoContent)
+                {
+                    List<Item> items = await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<List<Item>>(response.Content.ToString()));
+                    Items = new ObservableCollection<ItemViewModel>();
+                    foreach (Item i in items)
+                        Items.Add(new ItemViewModel(i));
+                    return true;
+                }
             }
             return false;
         }
         public static async Task<ItemViewModel> GetItemAsync(int Id)
         {
-            //await GetDataAsync(true);
+            await GetItemsAsync(0, true);
             if (Items.Count > 0)
-                return Items.Single(x => x.Model.Id == Id);
-
+                return Items.Single(x => x.Model.Id == Id); //TODO: Replace by a dictionary for faster search.
             return null;
         }
 
-        private static async Task<bool> GetDataAsync(bool singleItem = false)
-        {
-            if (singleItem || !Helpers.IsConnectedToInternet())
-                return await RestoreItemsAsync();
-            return await GetItemsAsync();
-        }
+
 
         private static async Task<bool> SaveItemsAsync()
         {
