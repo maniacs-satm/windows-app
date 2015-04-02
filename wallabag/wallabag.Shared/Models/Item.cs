ae2355667462b02c798e5656aa41206ad08b530a
Julian Oster <github@jlnostr.de>
2015-04-02 14:18:46 +0200

    Add Item model.

---
+++ wallabag/wallabag.Shared/Models/Item.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using PropertyChanged;
+using System;
+
+namespace wallabag.Models
+{
+    [ImplementPropertyChanged]
+    public class Item
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("is_archived")]
+        public bool IsArchived { get; set; }
+
+        [JsonProperty("is_starred")]
+        public bool IsStarred { get; set; }
+
+        [JsonProperty("is_deleted")]
+        public bool IsDeleted { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty("user")]
+        public User User { get; set; }
+    }
+}
