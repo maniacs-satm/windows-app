aa02302ee9f30320835d2f8cf9096b90d96269ff
Julian Oster <github@jlnostr.de>
2015-04-03 23:41:28 +0200

    Some UI improvements and the addition of the MainViewModel.

--- wallabag/wallabag.Shared/ViewModels/MainViewModel.cs
+++ wallabag/wallabag.Shared/ViewModels/MainViewModel.cs
@@ -3,11 +3,26 @@ using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using wallabag.Common;
 using wallabag.DataModel;
+using PropertyChanged;
 
 namespace wallabag.ViewModels
 {
+    [ImplementPropertyChanged]
     public class MainViewModel
     {
+        public ObservableCollection<ItemViewModel> Items { get; set; }
 
+        public RelayCommand RefreshCommand { get; set; }
+        private async Task Refresh()
+        {
+            // TODO: Improve the method handling and show some progress on the UI.
+            await DataSource.GetItemsAsync();
+            Items = DataSource.Items;
+        }
+
+        public MainViewModel()
+        {
+            RefreshCommand = new RelayCommand(async () => await Refresh());
+        }
     }
 }
