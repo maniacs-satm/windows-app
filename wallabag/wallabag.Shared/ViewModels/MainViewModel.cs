ff4b8c44a84f01732a8a58ae63ce1c6bdebedda1
Julian Oster <github@jlnostr.de>
2015-04-03 12:12:25 +0200

    Clean up MainViewModel.cs

--- wallabag/wallabag.Shared/ViewModels/MainViewModel.cs
+++ wallabag/wallabag.Shared/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using wallabag.Common;
 using wallabag.DataModel;
@@ -7,16 +8,6 @@ namespace wallabag.ViewModels
 {
     public class MainViewModel
     {
-        public RelayCommand refreshCommand { get; private set; }
-        private async Task RefreshItems()
-        {
-            var items = await wallabagDataSource.GetItemsAsync();
-        }
 
-        public MainViewModel()
-        {
-            refreshCommand = new RelayCommand(async () => await RefreshItems());
-            refreshCommand.Execute(0);
-        }
     }
 }
