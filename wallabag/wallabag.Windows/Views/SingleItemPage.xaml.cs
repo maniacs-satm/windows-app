66d14b4e81e4b35f1bc391d9d85cb621bd12f701
Julian Oster <github@jlnostr.de>
2015-04-04 17:10:33 +0200

    Update the SingleItemPage.

--- wallabag/wallabag.Windows/Views/SingleItemPage.xaml.cs
+++ wallabag/wallabag.Windows/Views/SingleItemPage.xaml.cs
@@ -1,33 +1,27 @@
-﻿using wallabag.Common;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using wallabag.ViewModels;
-
-// Die Elementvorlage "Standardseite" ist unter http://go.microsoft.com/fwlink/?LinkId=234237 dokumentiert.
 
 namespace wallabag.Views
 {
-    /// <summary>
-    /// Eine Standardseite mit Eigenschaften, die die meisten Anwendungen aufweisen.
-    /// </summary>
-    public sealed partial class SingleItemPage : basicPage
+    public sealed partial class SingleItemPage : Common.basicPage
     {
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        public SingleItemPage()
         {
-            DataContext = new SingleItemPageViewModel((int)e.Parameter);
+            this.InitializeComponent();
+        }
+        
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter != null)
+                DataContext = new ViewModels.SingleItemPageViewModel() { CurrentItem = await wallabag.DataModel.DataSource.GetItemAsync((int)e.Parameter) };
             base.OnNavigatedTo(e);
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+                Frame.GoBack();
         }
     }
 }
