eead1dcba3e7a0f1ff17c21bacab6b2b8e727b77
Julian Oster <github@jlnostr.de>
2015-04-04 17:18:41 +0200

    Add the ContentWithTitle property and the GenerateCSS() method to the ItemViewModel.

--- wallabag/wallabag.Shared/DataModel/ItemViewModel.cs
+++ wallabag/wallabag.Shared/DataModel/ItemViewModel.cs
@@ -5,15 +5,57 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using wallabag.Common;
+using Windows.UI;
 using Windows.Web.Http;
 
 namespace wallabag.DataModel
 {
     [ImplementPropertyChanged]
-    public class ItemViewModel
+    public class ItemViewModel : ViewModelBase
     {
         public Item Model { get; set; }
         public ObservableCollection<string> Tags { get; set; }
+
+        public string ContentWithTitle
+        {
+            get
+            {
+                var content =
+                "<html><head><link rel=\"stylesheet\" href=\"ms-appx-web:///Assets/css/wallabag.css\" type=\"text/css\" media=\"screen\" />" + GenerateCSS() + "</head>" +
+                "<h1 class=\"wallabag-header\">" + Model.Title + "</h1>" +
+                Model.Content +
+                "</html>";
+                return content;
+            }
+        }
+        private string GenerateCSS()
+        {
+            string css = "body {" +
+                CSSproperty("font-size", AppSettings.FontSize + "px") +
+                CSSproperty("line-height", AppSettings.LineHeight.ToString().Replace(",", ".")) +
+                CSSproperty("color", AppSettings.TextColor) +
+                CSSproperty("background", AppSettings.BackgroundColor) +
+#if WINDOWS_APP
+                CSSproperty("max-width", "960px") +
+                CSSproperty("margin", "0 auto") +
+                CSSproperty("padding", "0 20px") +
+#endif
+ "}";
+            return "<style>" + css + "</style>";
+        }
+        private string CSSproperty(string name, object value)
+        {
+            if (value.GetType() != typeof(Color))
+            {
+                return string.Format("{0}: {1};", name, value.ToString());
+            }
+            else
+            {
+                var color = (Color)value;
+                var tmpColor = string.Format("rgba({0}, {1}, {2}, {3})", color.R, color.G, color.B, color.A);
+                return string.Format("{0}: {1};", name, tmpColor);
+            }
+        }
 
         public ItemViewModel(Item Model)
         {
