0e2402afccf022653c1ef38c73ed8caa8a9fa8f3
Julian Oster <github@jlnostr.de>
2015-04-09 11:05:17 +0200

    Add animation for the RefreshButton and handle the different menu actions.

--- wallabag/wallabag.Shared/ViewModels/MainViewModel.cs
+++ wallabag/wallabag.Shared/ViewModels/MainViewModel.cs
@@ -5,22 +5,54 @@ using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using wallabag.Common;
 using wallabag.DataModel;
+using System.Linq;
 
 namespace wallabag.ViewModels
 {
     [ImplementPropertyChanged]
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
         public ObservableCollection<ItemViewModel> Items { get; set; }
+        public ObservableCollection<ItemViewModel> CurrentItems { get; set; }
+        private ObservableCollection<ItemViewModel> UnreadItems
+        {
+            get
+            {
+                return new ObservableCollection<ItemViewModel>(Items.Where(x => x.Model.IsStarred == false &&
+                    x.Model.IsArchived == false &&
+                    x.Model.IsDeleted == false));
+            }
+        }
+        private ObservableCollection<ItemViewModel> FavoriteItems
+        {
+            get
+            {
+                return new ObservableCollection<ItemViewModel>(Items.Where(x => x.Model.IsStarred == true &&
+                    x.Model.IsDeleted == false));
+            }
+        }
+        private ObservableCollection<ItemViewModel> ArchivedItems
+        {
+            get
+            {
+                return new ObservableCollection<ItemViewModel>(Items.Where(x => x.Model.IsArchived == true &&
+                    x.Model.IsDeleted == false));
+            }
+        }
+
         public ItemViewModel CurrentItem { get; set; }
 
         public RelayCommand RefreshCommand { get; set; }
         private async Task Refresh()
         {
-            // TODO: Improve the method handling and show some progress on the UI.
+            IsActive = true;
             if (await DataSource.GetItemsAsync())
+            {
                 foreach (var item in DataSource.Items)
                     Items.Add((ItemViewModel)item.Value);
+                RaisePropertyChanged("Items");
+            }
+            IsActive = false;
         }
 
         public RelayCommand DeleteCommand { get; set; }
@@ -28,14 +60,26 @@ namespace wallabag.ViewModels
         {
             bool success = await CurrentItem.Delete();
             if (success)
+            {
                 Items.Remove(CurrentItem);
+                CurrentItem = null;
+            }
+
         }
+
+        public RelayCommand ShowUnreadItems { get; set; }
+        public RelayCommand ShowFavoriteItems { get; set; }
+        public RelayCommand ShowArchivedItems { get; set; }
 
         public MainViewModel()
         {
             Items = new ObservableCollection<ItemViewModel>();
             RefreshCommand = new RelayCommand(async () => await Refresh());
             DeleteCommand = new RelayCommand(async () => await Delete());
+
+            ShowUnreadItems = new RelayCommand(() => { CurrentItems = UnreadItems; });
+            ShowFavoriteItems = new RelayCommand(() => { CurrentItems = FavoriteItems; });
+            ShowArchivedItems = new RelayCommand(() => { CurrentItems = ArchivedItems; });
         }
     }
 }
