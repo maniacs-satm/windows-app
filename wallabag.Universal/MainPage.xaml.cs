f4ba5c7dd464656b5cd3ae0a330e65b32cb184fd
Julian Oster <github@jlnostr.de>
2015-04-19 13:30:29 +0200

    typo

--- wallabag.Universal/MainPage.xaml.cs
+++ wallabag.Universal/MainPage.xaml.cs
@@ -30,7 +30,7 @@ namespace wallabag.Universal
         private void ItemsMenuButton_Click(object sender, RoutedEventArgs e)
         {
             HeaderTextBlock.Text = "Items";
-            itemGrid.Visibility = Visibility.Visible; // ensure that the itemGrid is visible, even if the FavoriteMenuButton is selected
+            itemGrid.Visibility = Visibility.Visible; // ensure that the itemGrid is visible, even if the FavoriteMenuButton is clicked
             BottomAppBar.Visibility = Visibility.Visible;
         }
 
