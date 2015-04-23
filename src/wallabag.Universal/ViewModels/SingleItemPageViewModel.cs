f6414c05dbfc3bb85b0e5ee35a234332520f7774
Julian Oster <github@jlnostr.de>
2015-04-23 15:49:46 +0200

    Added possibility to download the current item in different file formats (PDF, EPUB, MOBI).

--- src/wallabag.Universal/ViewModels/SingleItemPageViewModel.cs
+++ src/wallabag.Universal/ViewModels/SingleItemPageViewModel.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Text;
 using PropertyChanged;
 using wallabag.DataModel;
+using wallabag.Common;
+using Windows.System;
 
 namespace wallabag.ViewModels
 {
     [ImplementPropertyChanged]
-    class SingleItemPageViewModel
+    class SingleItemPageViewModel : ViewModelBase
     {
         public ItemViewModel CurrentItem { get; set; }
+
+        public RelayCommand DownloadItemAsPDFCommand { get; private set; }
+        public RelayCommand DownloadItemAsMobiCommand { get; private set; }
+        public RelayCommand DownloadItemAsEpubCommand { get; private set; }
         public SingleItemPageViewModel()
         {
-
+            DownloadItemAsPDFCommand = new RelayCommand(async () => { await Launcher.LaunchUriAsync(new Uri($"{AppSettings.WallabagUrl}/view/{CurrentItem.Model.Id}?pdf&method=id&value={CurrentItem.Model.Id}")); });
+            DownloadItemAsMobiCommand = new RelayCommand(async () => { await Launcher.LaunchUriAsync(new Uri($"{AppSettings.WallabagUrl}/view/{CurrentItem.Model.Id}?mobi&method=id&value={CurrentItem.Model.Id}")); });
+            DownloadItemAsEpubCommand = new RelayCommand(async () => { await Launcher.LaunchUriAsync(new Uri($"{AppSettings.WallabagUrl}/view/{CurrentItem.Model.Id}?epub&method=id&value={CurrentItem.Model.Id}")); });
         }
     }
 }
