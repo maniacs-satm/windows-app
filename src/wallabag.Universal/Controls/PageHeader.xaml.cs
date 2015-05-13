b51f3454c2839b6a4659fc885e1535df838705bc
Julian Oster <github@jlnostr.de>
2015-05-13 18:36:33 +0200

    Add the PageHeader control.

---
+++ src/wallabag.Universal/Controls/PageHeader.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Die Elementvorlage "Benutzersteuerelement" ist unter http://go.microsoft.com/fwlink/?LinkId=234236 dokumentiert.
+
+namespace wallabag.Controls
+{
+    public sealed partial class PageHeader : UserControl
+    {
+        public PageHeader()
+        {
+            InitializeComponent();
+        }
+
+        public UIElement HeaderContent
+        {
+            get { return (UIElement)GetValue(HeaderContentProperty); }
+            set { SetValue(HeaderContentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HeaderContent.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderContentProperty =
+            DependencyProperty.Register("HeaderContent", typeof(UIElement), typeof(PageHeader), new PropertyMetadata(DependencyProperty.UnsetValue));
+    }
+}
