501d3a1f57f1f60e6b144dd72e3b9bbca2646c66
Julian Oster <github@jlnostr.de>
2015-05-13 20:14:44 +0200

    Add ProgressRing to ContentPage and handle the ItemClick event.

--- src/wallabag.Universal/Views/ContentPage.xaml.cs
+++ src/wallabag.Universal/Views/ContentPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using wallabag.DataModel;
+using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -12,6 +13,11 @@ namespace wallabag.Views
         public ContentPage()
         {
             this.InitializeComponent();
+        }
+
+        private void ItemListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            WebView.NavigateToString((e.ClickedItem as ItemViewModel).ContentWithHeader);
         }
     }
 }
