84082398290fc26126864d107db0acd0ddad3c2e
Julian Oster <github@jlnostr.de>
2015-05-13 18:57:31 +0200

    Add basic navigation handling to AppShell.

--- src/wallabag.Universal/AppShell.xaml.cs
+++ src/wallabag.Universal/AppShell.xaml.cs
@@ -1,8 +1,7 @@
-﻿using wallabag.Common;
-using wallabag.DataModel;
-using wallabag.ViewModels;
+﻿using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -13,9 +12,77 @@ namespace wallabag.Universal
     /// </summary>
     public sealed partial class AppShell : Page
     {
+        public Frame AppFrame { get { return this.frame; } }
+
         public AppShell()
         {
             this.InitializeComponent();
+
+            SystemNavigationManager.GetForCurrentView().BackRequested += AppShell_BackRequested;
+            AppFrame.Navigate(typeof(Views.ContentPage));
+        }
+
+        #region BackRequested Handlers
+
+        private void AppShell_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            bool handled = e.Handled;
+            this.BackRequested(ref handled);
+            e.Handled = handled;
+        }
+
+        private void BackRequested(ref bool handled)
+        {
+            // Get a hold of the current frame so that we can inspect the app back stack.
+
+            if (this.AppFrame == null)
+                return;
+
+            // Check to see if this is the top-most page on the app back stack.
+            if (this.AppFrame.CanGoBack && !handled)
+            {
+                // If not, set the event to handled and go back to the previous page in the app.
+                handled = true;
+                this.AppFrame.GoBack();
+            }
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool ignored = false;
+            this.BackRequested(ref ignored);
+        }
+
+        #endregion
+
+        #region Navigation
+
+        private void OnNavigatedToPage(object sender, NavigationEventArgs e)
+        {
+            // After a successful navigation set keyboard focus to the loaded page
+            if (e.Content is Page && e.Content != null)
+            {
+                var control = (Page)e.Content;
+                control.Loaded += Page_Loaded;
+            }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ((Page)sender).Focus(FocusState.Programmatic);
+            ((Page)sender).Loaded -= Page_Loaded;
+        }
+
+        #endregion
+
+        private void UnreadItemsMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.Navigate(typeof(Views.ContentPage));
+        }
+
+        private void SettingsMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.Navigate(typeof(Views.SettingsPage));
         }
     }
 }
