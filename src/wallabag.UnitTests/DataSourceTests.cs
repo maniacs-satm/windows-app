658fb247bb123cafd9b0eab51da67ad386829982
Julian Oster <github@jlnostr.de>
2015-06-10 20:46:39 +0200

    Remove useless and not longer working tests.

--- src/wallabag.UnitTests/DataSourceTests.cs
+++ src/wallabag.UnitTests/DataSourceTests.cs
@@ -22,34 +22,6 @@ namespace wallabag.UnitTests
         }
 
         [TestMethod]
-        public async Task GetSaveAndRestoreItems()
-        {
-            await GetItems();
-            await SaveItems();
-            await RestoreItems();
-            await RestoreItemsWithoutFile();
-        }
-
-        [TestMethod]
-        public async Task SaveItems()
-        {
-            Assert.AreEqual(true, await DataSource.SaveItemsAsync());
-        }
-
-        public async Task RestoreItems()
-        {
-            Assert.AreEqual(true, await DataSource.RestoreItemsAsync());
-            Assert.IsNotNull(DataSource.Items);
-        }
-
-        public async Task RestoreItemsWithoutFile()
-        {
-            StorageFile file = await ApplicationData.Current.LocalFolder.GetFileAsync("items.json");
-            await file.DeleteAsync();
-            Assert.AreEqual(false, await DataSource.RestoreItemsAsync());
-        }
-
-        [TestMethod]
         [DataRow("http://www.sueddeutsche.de/politik/g-gipfel-politik-nach-schlossherrenart-1.2508802", "politik")]
         [DataRow("http://www.sueddeutsche.de/politik/griechenland-nichts-gelernt-1.2507116", "griechenland,politik")]
         [DataRow("http://www.sueddeutsche.de/politik/wagenknecht-auf-linken-parteitag-in-bielefeld-gegen-die-luegner-aus-der-trueben-bruehe-1.2508967", "politik")]
