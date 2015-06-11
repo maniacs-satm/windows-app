abfd4b731814c326d476550464f71f4623cc7bce
Julian Oster <github@jlnostr.de>
2015-06-11 14:50:22 +0200

    Update MainViewModel and remove DeleteCommand and update the other commands.

--- src/wallabag.Universal/ViewModels/MainViewModel.cs
+++ src/wallabag.Universal/ViewModels/MainViewModel.cs
@@ -1,8 +1,6 @@
-﻿using PropertyChanged;
-using System;
-using System.Collections.ObjectModel;
-using System.Linq;
+﻿using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using PropertyChanged;
 using wallabag.Common.MVVM;
 using wallabag.DataModel;
 
@@ -11,48 +9,35 @@ namespace wallabag.ViewModels
     [ImplementPropertyChanged]
     public class MainViewModel : ViewModelBase
     {
-        public ObservableCollection<ItemViewModel> _Items { get { return DataSource.Items; } }
         public ObservableCollection<ItemViewModel> Items { get; set; }
 
         public ItemViewModel CurrentItem { get; set; }
         public bool CurrentItemIsNotNull { get { return CurrentItem != null; } }
 
-        public RelayCommand RefreshCommand { get; private set; }
-        private async Task Refresh()
+        private async Task LoadItems(DataSource.ItemType itemType)
         {
             IsActive = true;
-            if (await DataSource.GetItemsAsync())
-            {
-                RaisePropertyChanged(nameof(Items));
-                ShowUnreadItems.Execute(0);
-            }
+            foreach (Item i in await DataSource.GetItemsAsync(itemType))
+                Items.Add(new ItemViewModel(i));
             IsActive = false;
         }
 
-        public RelayCommand DeleteCommand { get; private set; }
-        private async Task Delete()
-        {
-            bool success = await CurrentItem.Delete();
-            if (success)
-            {
-                _Items.Remove(CurrentItem);
-                CurrentItem = null;
-            }
-
-        }
-
+        public RelayCommand RefreshCommand { get; private set; }
         public RelayCommand ShowUnreadItems { get; private set; }
         public RelayCommand ShowFavoriteItems { get; private set; }
         public RelayCommand ShowArchivedItems { get; private set; }
 
         public MainViewModel()
         {
-            RefreshCommand = new RelayCommand(async () => await Refresh());
-            DeleteCommand = new RelayCommand(async () => await Delete());
+            RefreshCommand = new RelayCommand(async () =>
+            {
+                await DataSource.RefreshItems();
+                await LoadItems(DataSource.ItemType.Unread);
+            });
 
-            ShowUnreadItems = new RelayCommand(() => { if (_Items != null) Items = new ObservableCollection<ItemViewModel>(_Items.Where(i => i.Model.IsArchived == false && i.Model.IsDeleted == false && i.Model.IsStarred == false)); });
-            ShowFavoriteItems = new RelayCommand(() => { if (_Items != null) Items = new ObservableCollection<ItemViewModel>(_Items.Where(i => i.Model.IsStarred == true && i.Model.IsDeleted == false)); });
-            ShowArchivedItems = new RelayCommand(() => { if (_Items != null) Items = new ObservableCollection<ItemViewModel>(_Items.Where(i => i.Model.IsArchived == true && i.Model.IsDeleted == false)); });
+            ShowUnreadItems = new RelayCommand(async () => await LoadItems(DataSource.ItemType.Unread));
+            ShowFavoriteItems = new RelayCommand(async () => await LoadItems(DataSource.ItemType.Favorites));
+            ShowArchivedItems = new RelayCommand(async () => await LoadItems(DataSource.ItemType.Archived));
         }
     }
 }
