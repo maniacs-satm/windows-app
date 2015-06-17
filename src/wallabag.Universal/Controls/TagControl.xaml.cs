cd10de9de8eef8d72c0244a6d2a7204998982cd6
Julian Oster <github@jlnostr.de>
2015-06-17 07:26:49 +0200

    Update the TagControl.

--- src/wallabag.Universal/Controls/TagControl.xaml.cs
+++ src/wallabag.Universal/Controls/TagControl.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using wallabag.DataModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,6 +23,35 @@ namespace wallabag.Controls
         public TagControl()
         {
             this.InitializeComponent();
+        }
+
+        public ICollection<Tag> Tags
+        {
+            get { return (ICollection<Tag>)GetValue(TagsProperty); }
+            set { SetValue(TagsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Tags.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TagsProperty =
+            DependencyProperty.Register("Tags", typeof(ICollection<Tag>), typeof(TagControl), new PropertyMetadata(DependencyProperty.UnsetValue));
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var currentText = textBox.Text.ToLower();
+            char comma;
+            char.TryParse(",", out comma);
+            if (currentText.Length > 0 && currentText[currentText.Length - 1] == comma)
+            {
+                currentText = currentText.Remove(currentText.Length - 1);
+                Tags.Add(new Tag() { Label = currentText });
+                textBox.Text = string.Empty;
+            }
+
+        }
+
+        private void listView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Tags.Remove(e.ClickedItem as Tag);
         }
     }
 }
