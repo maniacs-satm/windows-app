cb5bd665998457c0edc91e1291ad79c4bafcf9bd
Julian Oster <github@jlnostr.de>
2015-08-10 22:12:07 +0200

    Update scripts.js to disable the possibility of the change of the line height.

--- src/wallabag.Universal/Assets/Article/scripts.js
+++ src/wallabag.Universal/Assets/Article/scripts.js
@@ -10,17 +10,17 @@ function changeHtmlAttributes(color, font, fontsize, lineheight) {
     document.getElementsByTagName("html")[0].setAttribute("data-font", font);
 
     document.body.style.fontSize = parseFloat(fontsize) + "px";
-    document.body.style.lineHeight = parseFloat(lineheight);
+    //document.body.style.lineHeight = parseFloat(lineheight);
 }
 
 function Initialize() {
     var root = document.getElementsByTagName("html")[0];
     var fontSize = root.getAttribute("data-font-size");
-    var lineHeight = root.getAttribute("data-line-height");
+    //var lineHeight = root.getAttribute("data-line-height");
     var progress = root.getAttribute("data-progress");
 
     document.body.style.fontSize = parseFloat(fontSize) + "px";
-    document.body.style.lineHeight = parseFloat(lineHeight);
+    //document.body.style.lineHeight = parseFloat(lineHeight);
 
     var progressCalculated = progress / 100 * (root.scrollHeight - root.clientHeight);
     document.body.scrollTop = progressCalculated;    
