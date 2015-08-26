9908ef6549f258038910a3c2b51cbdf022884d9e
Julian Oster <github@jlnostr.de>
2015-08-26 22:50:43 +0200

    Add first version of the actions for multiple selected items.

--- src/wallabag.Universal/Views/ContentPage.xaml.cs
+++ src/wallabag.Universal/Views/ContentPage.xaml.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using wallabag.Common;
+using wallabag.Models;
 using wallabag.Services;
 using wallabag.ViewModels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Navigation;
 
 namespace wallabag.Views
@@ -17,6 +17,7 @@ namespace wallabag.Views
         public MainViewModel ViewModel { get { return (MainViewModel)DataContext; } }
         public ObservableCollection<string> SearchBoxSuggestions { get; set; } = new ObservableCollection<string>();
         public ObservableCollection<string> TitleList { get; set; } = new ObservableCollection<string>();
+        public ObservableCollection<Tag> MultipleSelectionTags { get; set; }
 
         public ContentPage()
         {
@@ -66,7 +67,45 @@ namespace wallabag.Views
                 await AddItemContentDialog.ShowAsync();
         }
 
-        private void multipleSelectToggleButton_Checked(object sender, RoutedEventArgs e) { ItemGridView.SelectionMode = ListViewSelectionMode.Multiple; }
-        private void multipleSelectToggleButton_Unchecked(object sender, RoutedEventArgs e) { ItemGridView.SelectionMode = ListViewSelectionMode.None; }
+        private void multipleSelectToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ItemGridView.SelectionMode = ListViewSelectionMode.Multiple;
+            acceptAppBarButton.Visibility = Visibility.Visible;
+            favoriteAppBarButton.Visibility = Visibility.Visible;
+            tagAppBarButton.Visibility = Visibility.Visible;
+            deleteAppBarButton.Visibility = Visibility.Visible;
+            filterToggleButton.Visibility = Visibility.Collapsed;
+            addItemAppBarButton.Visibility = Visibility.Collapsed;
+            syncAppBarButton.Visibility = Visibility.Collapsed;
+        }
+        private void multipleSelectToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ItemGridView.SelectionMode = ListViewSelectionMode.None;
+            acceptAppBarButton.Visibility = Visibility.Collapsed;
+            favoriteAppBarButton.Visibility = Visibility.Collapsed;
+            tagAppBarButton.Visibility = Visibility.Collapsed;
+            deleteAppBarButton.Visibility = Visibility.Collapsed;
+            filterToggleButton.Visibility = Visibility.Visible;
+            addItemAppBarButton.Visibility = Visibility.Visible;
+            syncAppBarButton.Visibility = Visibility.Visible;
+        }
+
+        private async void acceptAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (ItemViewModel item in ItemGridView.SelectedItems)
+                await item.SwitchReadValueAsync();
+        }
+
+        private async void favoriteAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (ItemViewModel item in ItemGridView.SelectedItems)
+                await item.SwitchFavoriteValueAsync();
+        }
+
+        private async void deleteAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (ItemViewModel item in ItemGridView.SelectedItems)
+                await item.DeleteItemAsync();
+        }
     }
 }
