f96ff6b5a50775e9de99b3d4d9777b903c9bfb4b
Julian Oster <github@jlnostr.de>
2015-09-02 21:08:20 +0200

    Add ShareTargetPage to project.

---
+++ src/wallabag.Universal/Views/ShareTarget.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using wallabag.Common;
+using wallabag.Common.Mvvm;
+using wallabag.Models;
+using wallabag.Services;
+using Windows.ApplicationModel.DataTransfer.ShareTarget;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace wallabag.Views
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class ShareTarget : Page
+    {
+        private ShareOperation shareOperation;
+
+        public string Url { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+
+        public Command AddItemCommand { get; private set; }
+        public Command CancelCommand { get; private set; }
+
+        public ShareTarget()
+        {
+            InitializeComponent();
+            AddItemCommand = new Command(async () =>
+            {
+                await DataService.AddItemAsync(Url, Tags.ToCommaSeparatedString());
+                shareOperation.ReportCompleted();
+            });
+            CancelCommand = new Command(() => { shareOperation.DismissUI(); });
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            shareOperation = (ShareOperation)e.Parameter;
+            Url = (await shareOperation.Data.GetWebLinkAsync()).ToString();
+        }
+    }
+}
