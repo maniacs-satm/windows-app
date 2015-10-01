c3f770a508479c148bfa72b666dae04b14a4f4fa
Julian Oster <github@jlnostr.de>
2015-10-01 22:35:58 +0200

    Change the icon dynamically based on model properties.

---
+++ src/wallabag.Universal/Converter/ReadToIconConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace wallabag.Converter
+{
+    public sealed class ReadToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value is bool && (bool)value) ? new SymbolIcon(Symbol.Add) : new SymbolIcon(Symbol.Accept);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
