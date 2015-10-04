feef81553c21ab40ae2383a6baf1cb96c7d67ecf
Julian Oster <github@jlnostr.de>
2015-10-04 15:41:58 +0200

    Add SyncItemsBackgroundTask placeholder.

---
+++ src/wallabag.Tasks/SyncItemsBackgroundTask.cs
@@ -0,0 +1,21 @@
+﻿using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
+
+namespace wallabag.Tasks
+{
+    public sealed class SyncItemsBackgroundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            taskInstance.Canceled += TaskInstance_Canceled;
+            // TODO: Update live tile
+            // TODO: Sync items
+        }
+
+        private void TaskInstance_Canceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            System.Diagnostics.Debug.WriteLine("Cancel reason: " + reason.ToString());
+        }
+    }
+
+}
