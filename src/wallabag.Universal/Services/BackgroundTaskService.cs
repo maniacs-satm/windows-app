2a57bcb8dbd0c5131576fc3189a495d852a318ac
Julian Oster <github@jlnostr.de>
2015-10-04 15:45:12 +0200

    Add BackgroundTaskService to wallabag.Services.

---
+++ src/wallabag.Universal/Services/BackgroundTaskService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace wallabag.Services
+{
+    class BackgroundTaskService
+    {
+        public static async Task<BackgroundTaskRegistration> RegisterSyncItemsBackgroundTask()
+        {
+            await BackgroundExecutionManager.RequestAccessAsync();
+
+            var builder = new BackgroundTaskBuilder();
+
+            builder.Name = "SyncItemsBackgroundTask";
+            builder.TaskEntryPoint = "wallabag.Tasks.SyncItemsBackgroundTask";
+
+            IBackgroundTrigger timeTrigger = new TimeTrigger(15, false); // TODO: Add option for selecting the time span
+            builder.SetTrigger(timeTrigger);
+            builder.AddCondition(new SystemCondition(SystemConditionType.InternetAvailable));
+
+            BackgroundTaskRegistration task = builder.Register();
+
+            return task;
+        }
+
+        public static void UnregisterSyncItemsBackgroundTask()
+        {
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+                if (cur.Value.Name == "SyncItemsBackgroundTask")
+                    cur.Value.Unregister(true);
+        }
+    }
+}
