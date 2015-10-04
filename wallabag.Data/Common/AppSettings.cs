072e774a1ed2480f19f07b63d6dadf68db1eba9a
Julian Oster <github@jlnostr.de>
2015-10-04 19:35:35 +0200

    Add files from the Common folder.

---
+++ wallabag.Data/Common/AppSettings.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using PropertyChanged;
+using Windows.Security.Credentials;
+using Windows.Storage;
+
+namespace wallabag.Common
+{
+    /// <summary>
+    /// This class provides easy access to the roaming settings (sync over multiple devices).
+    /// </summary>
+    [ImplementPropertyChanged]
+    public class AppSettings
+    {
+        #region General things
+
+        private static ApplicationDataContainer roamingSettings = ApplicationData.Current.RoamingSettings;
+        private static ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+
+        /// <summary>
+        /// To inform the View about a changed property, we raise a PropertyChangedEvent.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
+        private static void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                PropertyChanged.Invoke(e, e);
+            }
+        }
+        public static event PropertyChangedEventHandler PropertyChanged;
+
+        private static T GetProperty<T>(string key, T defaultValue = default(T), bool SaveLocal = false)
+        {
+            if (SaveLocal)
+                return (T)(localSettings.Values[key] ?? defaultValue);
+            else
+                return (T)(roamingSettings.Values[key] ?? defaultValue);
+        }
+        private static void SetProperty(string key, object value, bool IsSavedLocal = false)
+        {
+            if (IsSavedLocal)
+                localSettings.Values[key] = value;
+            else
+                roamingSettings.Values[key] = value;
+            RaisePropertyChanged(key);
+        }
+        #endregion
+
+        #region PasswordVault
+        private static void GetFromPasswordVault()
+        {
+            try
+            {
+                PasswordVault vault = new PasswordVault();
+                IReadOnlyList<PasswordCredential> creds = vault.RetrieveAll();
+
+                if (creds != null && creds.Count != 0)
+                {
+                    creds[0].RetrievePassword();
+                    _Username = creds[0].UserName;
+                    _Password = creds[0].Password;
+                    _wallabagUrl = creds[0].Resource;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        private static void SaveToPasswordVault()
+        {
+            if (!string.IsNullOrWhiteSpace(_Username) &&
+                !string.IsNullOrWhiteSpace(_Password) &&
+                !string.IsNullOrWhiteSpace(_wallabagUrl))
+            {
+                PasswordVault vault = new PasswordVault();
+                PasswordCredential cred = new PasswordCredential(_wallabagUrl, _Username, _Password);
+
+                if (vault.RetrieveAll().Count == 0)
+                    vault.Add(cred);
+                else
+                {
+                    vault.Remove(vault.RetrieveAll()[0]);
+                    vault.Add(cred);
+                }
+            }
+        }
+        #endregion
+
+        private static string _Username;
+        public static string Username
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_Username))
+                    GetFromPasswordVault();
+                return _Username;
+            }
+            set
+            {
+                _Username = value;
+                SaveToPasswordVault();
+
+                RaisePropertyChanged(nameof(Username));
+            }
+        }
+        private static string _Password;
+        public static string Password
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_Password))
+                    GetFromPasswordVault();
+                return _Password;
+            }
+            set
+            {
+                _Password = value;
+                SaveToPasswordVault();
+                RaisePropertyChanged(nameof(Password));
+            }
+        }
+        private static string _wallabagUrl;
+        public static string wallabagUrl
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_wallabagUrl)) GetFromPasswordVault();
+                return _wallabagUrl;
+            }
+            set
+            {
+                _wallabagUrl = value;
+                RaisePropertyChanged(nameof(wallabagUrl));
+                SaveToPasswordVault();
+            }
+        }
+
+        public static double FontSize
+        {
+            get { return GetProperty<double>(nameof(FontSize), 18, true); }
+            set { SetProperty(nameof(FontSize), value, true); }
+        }
+        public static string FontFamily
+        {
+            get { return GetProperty(nameof(FontFamily), "serif"); }
+            set { SetProperty(nameof(FontFamily), value); }
+        }
+        public static string ColorScheme
+        {
+            get { return GetProperty(nameof(ColorScheme), "light", true); }
+            set { SetProperty(nameof(ColorScheme), value, true); }
+        }
+        public static string TextAlignment
+        {
+            get { return GetProperty(nameof(TextAlignment), "left"); }
+            set { SetProperty(nameof(TextAlignment), value); }
+        }
+        
+        public static bool NavigateBackAfterReadingAnArticle
+        {
+            get { return GetProperty(nameof(NavigateBackAfterReadingAnArticle), true); }
+            set { SetProperty(nameof(NavigateBackAfterReadingAnArticle), value); }
+        }
+        public static bool SyncReadingProgress
+        {
+            get { return GetProperty(nameof(SyncReadingProgress), true); }
+            set { SetProperty(nameof(SyncReadingProgress), value); }
+        }
+        public static bool SyncOnStartup
+        {
+            get { return GetProperty(nameof(SyncOnStartup), true); }
+            set { SetProperty(nameof(SyncOnStartup), value); }
+        }
+        public static bool UseClassicContextMenuForMouseInput
+        {
+            get { return GetProperty(nameof(UseClassicContextMenuForMouseInput), true); }
+            set { SetProperty(nameof(UseClassicContextMenuForMouseInput), value); }
+        }
+    }
+}
