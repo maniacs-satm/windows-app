83e5ee37cd26771d563d6eb66ee8cd378f8b9539
Julian Oster <github@jlnostr.de>
2015-10-07 12:34:49 +0200

    First template of AuthorizationService class.

---
+++ src/wallabag.Universal/Services/AuthorizationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wallabag.Services
+{
+    class AuthorizationService
+    {
+        private const string _ClientID = "1_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4";
+        private const string _ClientSecret = "4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k";
+
+        private string _RefreshToken;
+        private string _oAuthToken;
+
+        public void RequestToken(string Username, string Password, string Url)
+        {
+
+        }
+        public void RefreshToken()
+        {
+
+        }
+    }
+}
