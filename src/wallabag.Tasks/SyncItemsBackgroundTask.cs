ec41b968c14dd137f9d6345c4b4294e66f9556aa
Julian Oster <github@jlnostr.de>
2015-10-09 16:25:23 +0200

    Update SyncItemsBackgroundTask.

--- src/wallabag.Tasks/SyncItemsBackgroundTask.cs
+++ src/wallabag.Tasks/SyncItemsBackgroundTask.cs
@@ -1,15 +1,23 @@
-﻿using Windows.ApplicationModel.Background;
+﻿using System;
+using NotificationsExtensions.Badges;
+using wallabag.Services;
+using Windows.ApplicationModel.Background;
 using Windows.UI.Notifications;
 
 namespace wallabag.Tasks
 {
     public sealed class SyncItemsBackgroundTask : IBackgroundTask
     {
-        public void Run(IBackgroundTaskInstance taskInstance)
+        public async void Run(IBackgroundTaskInstance taskInstance)
         {
             taskInstance.Canceled += TaskInstance_Canceled;
-            // TODO: Update live tile
-            // TODO: Sync items
+
+            if (await DataService.SyncWithServerAsync())
+            {
+                BadgeNumericNotificationContent badgeContent =
+                    new BadgeNumericNotificationContent(42); // Update this number with the number of new items since last opening
+                TileUpdateManager.CreateTileUpdaterForApplication().Update(new TileNotification(badgeContent.GetXml()));
+            }
         }
 
         private void TaskInstance_Canceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
