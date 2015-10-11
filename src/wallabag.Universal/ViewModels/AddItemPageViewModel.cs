1ebe0d1c3ed8dd4273b2fe9c13b6b697d848fe50
Julian Oster <github@jlnostr.de>
2015-10-11 16:52:43 +0200

    Update AddItemPageViewModel to fix bugs because some propertiers were null.

--- src/wallabag.Universal/ViewModels/AddItemPageViewModel.cs
+++ src/wallabag.Universal/ViewModels/AddItemPageViewModel.cs
@@ -11,29 +11,28 @@ namespace wallabag.ViewModels
     [ImplementPropertyChanged]
     public class AddItemPageViewModel : ViewModelBase
     {
-        public string Url { get; set; }
-        public ObservableCollection<Tag> Tags { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public ObservableCollection<Tag> Tags { get; set; } = new ObservableCollection<Tag>();
 
         public DelegateCommand AddItemCommand { get; private set; }
         public DelegateCommand CancelCommand { get; private set; }
 
         public AddItemPageViewModel()
         {
-            Tags = new ObservableCollection<Tag>();
             AddItemCommand = new DelegateCommand(async () =>
             {
                 await DataService.AddItemAsync(Url, Tags.ToCommaSeparatedString());
                 Url = string.Empty;
                 Tags.Clear();
 
-                if (NavigationService.CanGoBack)
+                if (NavigationService != null && NavigationService.CanGoBack)
                     NavigationService.GoBack();
             });
             CancelCommand = new DelegateCommand(() =>
             {
                 Url = string.Empty;
                 Tags.Clear();
-                if (NavigationService.CanGoBack)
+                if (NavigationService != null && NavigationService.CanGoBack)
                     NavigationService.GoBack();
             });
         }
