800b353482c8eaab5662a539289ab017c64578c9
Julian Oster <github@jlnostr.de>
2015-10-20 20:15:51 +0200

    Fucked up .gitignore. OfflineTask added.

---
+++ src/wallabag.Data/Models/OfflineTask.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+
+namespace wallabag.Models
+{
+    [Table("OfflineTasks")]
+    public class OfflineTask
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public int ItemId { get; set; }
+        public string TagsString { get; set; }
+        public int TagId { get; set; }
+        public string Url { get; set; }
+
+        public OfflineTaskType Task { get; set; }
+
+        public enum OfflineTaskType
+        {
+            AddItem,
+            DeleteItem,
+            AddTags,
+            DeleteTag,
+            MarkItemAsRead,
+            UnmarkItemAsRead,
+            MarkItemAsFavorite,
+            UnmarkItemAsFavorite
+        }
+    }
+}
