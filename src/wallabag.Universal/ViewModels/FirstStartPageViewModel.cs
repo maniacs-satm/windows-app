dee6a78ca742ffcbbe25dc92ef00410d0eb83cb8
Julian Oster <github@jlnostr.de>
2015-10-23 23:27:51 +0200

    Show the current progress of downloading on the content page.

--- src/wallabag.Universal/ViewModels/FirstStartPageViewModel.cs
+++ src/wallabag.Universal/ViewModels/FirstStartPageViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using PropertyChanged;
 using Template10.Mvvm;
 using wallabag.Common;
+using Windows.UI.Xaml.Navigation;
 
 namespace wallabag.ViewModels
 {
@@ -29,21 +31,46 @@ namespace wallabag.ViewModels
             {
                 StatusText = "Logged in.";
                 AppSettings.wallabagUrl = wallabagUrl;
-                StatusText = "Create database…";
-                await Services.DataService.InitializeDatabaseAsync();
-                NavigationService.Navigate(typeof(Views.ContentPage));
-                NavigationService.ClearHistory();
-
-                Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = false;
+                await SetupWallabagAndNavigateToContentPage();
             }
             else
                 StatusText = "Login failed.";
 
         }
 
+        public async Task SetupWallabagAndNavigateToContentPage()
+        {
+            StatusText = "Create database…";
+            await Services.DataService.InitializeDatabaseAsync();
+
+            StatusText = "Downloading articles from server…";
+            var downloadTask = Services.DataService.DownloadItemsFromServerAsync();
+            downloadTask.Progress = (s, p) =>
+            {
+                StatusText = $"Downloading item {p.CurrentItemIndex} of {p.TotalNumberOfItems}";
+                System.Diagnostics.Debug.WriteLine(StatusText);
+            };
+            downloadTask.Completed = (i, s) =>
+            {
+                StatusText = "Finished downloading.";
+            };
+            var result = await downloadTask;
+            NavigationService.Navigate(typeof(Views.ContentPage));
+            NavigationService.ClearHistory();
+
+            Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = false;
+        }
+
         public FirstStartPageViewModel()
         {
             LoginCommand = new DelegateCommand(async () => await Login());
+        }
+
+        public override async void OnNavigatedTo(object parameter, NavigationMode mode, IDictionary<string, object> state)
+        {
+            bool databaseDoesNotExist = await Windows.Storage.ApplicationData.Current.LocalFolder.TryGetItemAsync(Helpers.DATABASE_FILENAME) == null;
+            if (!string.IsNullOrEmpty(AppSettings.AccessToken) && databaseDoesNotExist)
+                await SetupWallabagAndNavigateToContentPage();
         }
     }
 }
