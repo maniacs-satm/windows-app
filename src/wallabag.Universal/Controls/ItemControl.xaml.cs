5fc40a82387099654bee74b0983ca24b9d46eec7
Julian Oster <github@jlnostr.de>
2015-10-24 22:32:39 +0200

    Add storyboards for showing/hiding the overlay and make use of them :)

--- src/wallabag.Universal/Controls/ItemControl.xaml.cs
+++ src/wallabag.Universal/Controls/ItemControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using wallabag.Common;
+﻿using System.Threading.Tasks;
+using wallabag.Common;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Animation;
 
@@ -12,11 +13,28 @@ namespace wallabag.Controls
             if (!Helpers.IsPhone)
                 ContextMenuGrid.PointerExited += (s, e) => HideTouchMenu();
             ContextMenuGrid.PointerWheelChanged += (s, e) => HideTouchMenu();
+            this.PointerEntered += BottomGrid_PointerEntered;
+            this.PointerExited += BottomGrid_PointerExited;
 
             foreach (AppBarButton button in stackPanel.Children)
                 button.Click += (s, e) => HideTouchMenu();
         }
 
+        private bool _PointerExited;
+        private void BottomGrid_PointerExited(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            _PointerExited = true;
+            HideOverlayStoryboard.Begin();
+        }
+        private async void BottomGrid_PointerEntered(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            _PointerExited = false;
+            await Task.Delay(300);
+
+            if (!_PointerExited)
+                ShowOverlayStoryboard.Begin();
+        }
+        
         private void HideTouchMenu() =>
             (ContextMenuGrid.Resources["HideContextMenu"] as Storyboard).Begin();
 
