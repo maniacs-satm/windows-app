0efe0a0eb6244f814e286cfc9a9b787356c4da92
Julian Oster <github@jlnostr.de>
2015-10-29 11:57:54 +0100

    Add ShareTextControl which serves a template for sharing :)

---
+++ src/wallabag.Universal/Controls/ShareTextControl.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace wallabag.Controls
+{
+    public sealed partial class ShareTextControl : UserControl
+    {
+        public ShareTextControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public void SetupControl(string Title, string Content, string DomainName)
+        {
+            TitleTextBlock.Text = Title;
+            ContentTextBlock.Text = Content;
+            DomainNameTextBlock.Text = DomainName;
+        }
+
+        public async Task<RenderTargetBitmap> CaptureToStreamAsync(IRandomAccessStream stream)
+        {
+            var renderTargetBitmap = new RenderTargetBitmap();
+            await renderTargetBitmap.RenderAsync(RootGrid);
+
+            var pixels = await renderTargetBitmap.GetPixelsAsync();
+
+            var logicalDpi = DisplayInformation.GetForCurrentView().LogicalDpi;
+            var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, stream);
+            encoder.SetPixelData(
+                BitmapPixelFormat.Bgra8,
+                BitmapAlphaMode.Ignore,
+                (uint)renderTargetBitmap.PixelWidth,
+                (uint)renderTargetBitmap.PixelHeight,
+                logicalDpi,
+                logicalDpi,
+                pixels.ToArray());
+
+            await encoder.FlushAsync();
+
+            return renderTargetBitmap;
+        }
+    }
+}
