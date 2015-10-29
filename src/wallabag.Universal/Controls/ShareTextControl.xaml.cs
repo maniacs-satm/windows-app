a1154d71276ac9e48a98f91497084ed812d3ff28
Julian Oster <github@jlnostr.de>
2015-10-29 13:00:43 +0100

    Render the ShareTextControl with the doubled size.

--- src/wallabag.Universal/Controls/ShareTextControl.xaml.cs
+++ src/wallabag.Universal/Controls/ShareTextControl.xaml.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Graphics.Display;
 using Windows.Graphics.Imaging;
-using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -69,7 +59,7 @@ namespace wallabag.Controls
         public async Task<RenderTargetBitmap> CaptureToStreamAsync(IRandomAccessStream stream)
         {
             var renderTargetBitmap = new RenderTargetBitmap();
-            await renderTargetBitmap.RenderAsync(RootGrid);
+            await renderTargetBitmap.RenderAsync(RootGrid, (int)RootGrid.ActualWidth * 2, (int)RootGrid.ActualHeight * 2);
 
             var pixels = await renderTargetBitmap.GetPixelsAsync();
 
