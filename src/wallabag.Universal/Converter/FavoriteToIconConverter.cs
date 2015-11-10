84be178000d8fe88469617ff48c47f4e29470a12
Julian Oster <github@jlnostr.de>
2015-11-10 21:56:32 +0100

    Use hearts instead of stars.
    The photos app does it, the wallabag website does it and twitter, too :D

--- src/wallabag.Universal/Converter/FavoriteToIconConverter.cs
+++ src/wallabag.Universal/Converter/FavoriteToIconConverter.cs
@@ -8,7 +8,8 @@ namespace wallabag.Converter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return (value is bool && (bool)value) ? new SymbolIcon(Symbol.UnFavorite) : new SymbolIcon(Symbol.Favorite);
+            System.Diagnostics.Debug.WriteLine("Convert favorite to icon: " + value.ToString());
+            return (value is bool && (bool)value) ? new FontIcon() { Glyph = "", FontFamily = new Windows.UI.Xaml.Media.FontFamily("Segoe MDL2 Assets") } : new FontIcon() { Glyph = "", FontFamily = new Windows.UI.Xaml.Media.FontFamily("Segoe MDL2 Assets") };
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
