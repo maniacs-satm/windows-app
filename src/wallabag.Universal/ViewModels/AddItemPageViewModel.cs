e135c9f6bffa7676b50587bca0f4e96d56668cc7
Julian Oster <github@jlnostr.de>
2016-01-31 15:15:25 +0100

    Update AddItemPageViewModel to make use of the dependency injection.

--- src/wallabag.Universal/ViewModels/AddItemPageViewModel.cs
+++ src/wallabag.Universal/ViewModels/AddItemPageViewModel.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using PropertyChanged;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using PropertyChanged;
 using Template10.Mvvm;
 using wallabag.Common;
+using wallabag.Data.Interfaces;
 using wallabag.Models;
-using wallabag.Services;
 using Windows.ApplicationModel.DataTransfer.ShareTarget;
-using Windows.UI.Xaml.Navigation;
 
 namespace wallabag.ViewModels
 {
     [ImplementPropertyChanged]
     public class AddItemPageViewModel : ViewModelBase
     {
+        private IDataService _dataService;
+
         public string Url { get; set; } = string.Empty;
         public ICollection<Tag> Tags { get; set; } = new ObservableCollection<Tag>();
         public bool IsActive { get; set; } = false;
@@ -22,12 +22,14 @@ namespace wallabag.ViewModels
         public DelegateCommand AddItemCommand { get; private set; }
         public DelegateCommand CancelCommand { get; private set; }
 
-        public AddItemPageViewModel()
+        public AddItemPageViewModel(IDataService dataService)
         {
+            _dataService = dataService;
+
             AddItemCommand = new DelegateCommand(async () =>
             {
                 IsActive = true;
-                await DataService.AddItemAsync(Url, Tags.ToCommaSeparatedString());
+                await _dataService.AddItemAsync(Url, Tags.ToCommaSeparatedString());
                 Url = string.Empty;
                 Tags.Clear();
                 IsActive = false;
