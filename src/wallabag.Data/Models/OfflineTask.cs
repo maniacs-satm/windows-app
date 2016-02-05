4bb7760b290df36510eab1d88e3338b06cf6190b
Julian Oster <github@jlnostr.de>
2016-02-05 21:28:37 +0100

    Add an OfflineTaskViewModel for the UI and update the OfflineTask model to save more data.

--- src/wallabag.Data/Models/OfflineTask.cs
+++ src/wallabag.Data/Models/OfflineTask.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using SQLite;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using SQLite;
 using Windows.Web.Http;
 using static wallabag.Common.Helpers;
 
-namespace wallabag.Models
+namespace wallabag.Data.Models
 {
     [Table("OfflineTasks")]
     public class OfflineTask
@@ -12,10 +12,39 @@ namespace wallabag.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
+        public int ActionId { get; set; }
+
         public string RequestUri { get; set; }
         public Dictionary<string, object> RequestParameters { get; set; }
         public HttpRequestMethod RequestMethod { get; set; }
 
+        public OfflineTask() {
+            RequestUri = string.Empty;
+            RequestParameters = new Dictionary<string, object>();
+            RequestMethod = HttpRequestMethod.Patch;
+        }
+
+        public enum Action
+        {
+            MarkAsRead = 0,
+            UnmarkAsRead = 1,
+            MarkAsFavorite = 2,
+            UnmarkAsFavorite = 3,
+            ModifyTags = 4,
+            Delete = 5
+        }
+
+        public static async Task AddTaskAsync(Item Item, Action action, string requestUri, Dictionary<string, object> parameters, HttpRequestMethod method = HttpRequestMethod.Patch)
+        {
+            var newTask = new OfflineTask();
+
+            newTask.RequestUri = requestUri;
+            newTask.RequestParameters = parameters;
+            newTask.RequestMethod = method;
+            newTask.ActionId = action.GetHashCode();
+
+            await new SQLiteAsyncConnection(DATABASE_PATH).InsertAsync(newTask);
+        }
         public async Task<bool> ExecuteAsync()
         {
             var response = await ExecuteHttpRequestAsync(RequestMethod, RequestUri, RequestParameters);
@@ -27,14 +56,6 @@ namespace wallabag.Models
             }
             else
                 return false;
-        }
-
-        public OfflineTask() { }
-        public OfflineTask(string requestUri, Dictionary<string, object> parameters, HttpRequestMethod method = HttpRequestMethod.Patch)
-        {
-            RequestUri = requestUri;
-            RequestParameters = parameters;
-            RequestMethod = method;
         }
     }
 }
---
+++ src/wallabag.Data/ViewModels/OfflineTaskViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PropertyChanged;
+using Template10.Mvvm;
+using wallabag.Data.Models;
+
+namespace wallabag.Data.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class OfflineTaskViewModel : ViewModelBase
+    {
+        public OfflineTask Model { get; set; }
+
+        public string ItemTitle { get; set; }
+        public string ActionLogo { get; set; }
+
+        public OfflineTaskViewModel(OfflineTask Model)
+        {
+            this.Model = Model;
+        }
+    }
+}
