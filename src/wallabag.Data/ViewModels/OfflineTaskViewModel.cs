665b2fccbbffd44a7ae8da395c6eff4c4c3251ea
Julian Oster <github@jlnostr.de>
2016-02-06 19:43:16 +0100

    Update OfflineTaskViewModel to update ItemTitle and ActionLogo based on the OfflineTask.

--- src/wallabag.Data/ViewModels/OfflineTaskViewModel.cs
+++ src/wallabag.Data/ViewModels/OfflineTaskViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using PropertyChanged;
+using System.Threading.Tasks;
 using Template10.Mvvm;
+using wallabag.Data.Interfaces;
 using wallabag.Data.Models;
 
 namespace wallabag.Data.ViewModels
@@ -7,14 +9,45 @@ namespace wallabag.Data.ViewModels
     [ImplementPropertyChanged]
     public class OfflineTaskViewModel : ViewModelBase
     {
+        private IDataService _dataService;
         public OfflineTask Model { get; set; }
 
         public string ItemTitle { get; set; }
         public string ActionLogo { get; set; }
 
-        public OfflineTaskViewModel(OfflineTask Model)
+        public OfflineTaskViewModel(IDataService dataService) { _dataService = dataService; }
+
+        public async Task InitializeAsync(OfflineTask Model)
         {
             this.Model = Model;
+            ItemTitle = (await _dataService.GetItemAsync(Model.ItemId)).Title;
+
+            switch (Model.Action)
+            {
+                case OfflineTask.OfflineTaskAction.MarkAsRead:
+                    ActionLogo = "\uE001";
+                    break;
+                case OfflineTask.OfflineTaskAction.UnmarkAsRead:
+                    ActionLogo = "\uE052";
+                    break;
+                case OfflineTask.OfflineTaskAction.MarkAsFavorite:
+                    ActionLogo = "\uE006";
+                    break;
+                case OfflineTask.OfflineTaskAction.UnmarkAsFavorite:
+                    ActionLogo = "\uE007";
+                    break;
+                case OfflineTask.OfflineTaskAction.ModifyTags:
+                    ActionLogo = "\uE1CB";
+                    break;
+                case OfflineTask.OfflineTaskAction.Delete:
+                    ActionLogo = "\uE107";
+                    break;
+                case OfflineTask.OfflineTaskAction.AddItem:
+                    ActionLogo = "\uE109";
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
