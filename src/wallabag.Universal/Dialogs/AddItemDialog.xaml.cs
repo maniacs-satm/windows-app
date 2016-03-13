3ccc9d693a2f61c6a3829b9dbd8f904e7a23b640
Julian Oster <github@jlnostr.de>
2016-03-13 16:21:30 +0100

    Add ViewModel to AddItemDialog.
    I don't know why I forgot it :D

--- src/wallabag.Universal/Dialogs/AddItemDialog.xaml.cs
+++ src/wallabag.Universal/Dialogs/AddItemDialog.xaml.cs
@@ -1,9 +1,12 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using wallabag.ViewModels;
+using Windows.UI.Xaml.Controls;
 
 namespace wallabag.Dialogs
 {
     public sealed partial class AddItemDialog : ContentDialog
     {
+        public AddItemPageViewModel ViewModel { get { return this.DataContext as AddItemPageViewModel; } }
+
         public AddItemDialog()
         {
             this.InitializeComponent();
