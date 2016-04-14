85f89ea64b03ccff9a1106381aaa6a89ff1022b2
Julian Oster <github@jlnostr.de>
2016-04-14 09:36:40 +0200

    Display PreviewText if the image hasn't been loaded yet.

--- src/wallabag.Universal/Controls/ItemControl.xaml.cs
+++ src/wallabag.Universal/Controls/ItemControl.xaml.cs
@@ -8,32 +8,40 @@ namespace wallabag.Controls
     public sealed partial class ItemControl : UserControl
     {
         private double WindowWidth { get { return Window.Current.CoreWindow.Bounds.Width; } }
+        private double AspectRatio { get; set; } = 1.5;
         public ItemViewModel ViewModel { get { return DataContext as ItemViewModel; } }
+        private bool _IsImageLoaded = false;
 
         public ItemControl()
         {
             InitializeComponent();
             SizeChanged += ItemControl_SizeChanged;
-            
+
             if (WindowWidth >= 720)
+            {
                 MetadataStackPanel.RequestedTheme = ElementTheme.Light;
-                       
+                PreviewText.Visibility = Visibility.Visible;
+            }
+
             RootImageSource.ImageOpened += (s, e) =>
             {
+                _IsImageLoaded = true;
                 if (WindowWidth >= 720)
                     MetadataStackPanel.RequestedTheme = ElementTheme.Dark;
+
+                if (AspectRatio == 1.5)
+                    PreviewText.Visibility = Visibility.Collapsed;
             };
         }
 
         private void ItemControl_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            var aspectRatio = Math.Round(e.NewSize.Width / e.NewSize.Height, 1);
-            if (aspectRatio < 1.5 && WindowWidth >= 720 || string.IsNullOrEmpty(ViewModel.Model.PreviewPictureUri))
+            AspectRatio = Math.Round(e.NewSize.Width / e.NewSize.Height, 1);
+            if (AspectRatio < 1.5 && WindowWidth >= 720 || string.IsNullOrEmpty(ViewModel.Model.PreviewPictureUri))
                 VisualStateManager.GoToState(this, nameof(TwoRows), false);
-            else
+            else if (_IsImageLoaded)
                 VisualStateManager.GoToState(this, nameof(Normal), false);
-
-
+            
             if (WindowWidth >= 720)
             {
                 if (string.IsNullOrEmpty(ViewModel.Model.PreviewPictureUri))
