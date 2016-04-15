d028f7f1cff782f8d10bc231de7728ddaf3ea5c0
Julian Oster <github@jlnostr.de>
2016-04-15 21:02:38 +0200

    Handle both states in code-behind, too.

--- src/wallabag.Universal/Controls/ItemControl.xaml.cs
+++ src/wallabag.Universal/Controls/ItemControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using wallabag.Common;
 using wallabag.ViewModels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -7,9 +8,10 @@ namespace wallabag.Controls
 {
     public sealed partial class ItemControl : UserControl
     {
+        public ItemViewModel ViewModel { get { return DataContext as ItemViewModel; } }
+
         private double WindowWidth { get { return Window.Current.CoreWindow.Bounds.Width; } }
         private double AspectRatio { get; set; } = 1.5;
-        public ItemViewModel ViewModel { get { return DataContext as ItemViewModel; } }
         private bool _IsImageLoaded = false;
 
         public ItemControl()
@@ -17,18 +19,25 @@ namespace wallabag.Controls
             InitializeComponent();
             SizeChanged += ItemControl_SizeChanged;
 
-            if (WindowWidth >= 720)
+            Loading += (s, e) =>
             {
-                MetadataStackPanel.RequestedTheme = ElementTheme.Light;
-                PreviewText.Visibility = Visibility.Visible;
-            }
+                if (AppSettings.ItemStyle == AppSettings.ItemControlStyle.Simple)
+                {
+                    WideComplex.StateTriggers.Clear();
+                    VisualStateManager.GoToState(this, nameof(Normal), false);
+                }
+                else
+                {
+                    Wide.StateTriggers.Clear();
+                    VisualStateManager.GoToState(this, nameof(NormalComplex), false);
+                }
+            };
 
             RootImageSource.ImageOpened += (s, e) =>
             {
                 _IsImageLoaded = true;
                 if (WindowWidth >= 720)
                     MetadataStackPanel.RequestedTheme = ElementTheme.Dark;
-
                 if (AspectRatio == 1.5)
                     PreviewText.Visibility = Visibility.Collapsed;
             };
@@ -37,15 +46,21 @@ namespace wallabag.Controls
         private void ItemControl_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             AspectRatio = Math.Round(e.NewSize.Width / e.NewSize.Height, 1);
-            if (AspectRatio < 1.5 && WindowWidth >= 720 || string.IsNullOrEmpty(ViewModel.Model.PreviewPictureUri))
+            if (AspectRatio < 1.5 && WindowWidth >= 720)
                 VisualStateManager.GoToState(this, nameof(TwoRows), false);
-            else if (_IsImageLoaded)
+            else if (_IsImageLoaded && AppSettings.ItemStyle == AppSettings.ItemControlStyle.Simple)
                 VisualStateManager.GoToState(this, nameof(Normal), false);
-            
+            else if (_IsImageLoaded && AppSettings.ItemStyle == AppSettings.ItemControlStyle.Complex)
+                VisualStateManager.GoToState(this, nameof(NormalComplex), false);
+
             if (WindowWidth >= 720)
             {
-                if (string.IsNullOrEmpty(ViewModel.Model.PreviewPictureUri))
+                if (string.IsNullOrEmpty(ViewModel.Model.PreviewPictureUri) &&
+                    AppSettings.ItemStyle == AppSettings.ItemControlStyle.Complex)
+                {
                     MetadataStackPanel.RequestedTheme = ElementTheme.Light;
+                    PreviewText.Visibility = Visibility.Visible;
+                }
             }
         }
     }
